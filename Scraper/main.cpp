2da2f87e75d8b4d75382081ff36eaccd105ebb18
Brian Kress <brian.kress@SFM3UMF6T6L.local>
2014-05-29 10:01:56 -0700

    Initial Commit

---
+++ Scraper/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Scraper
+//
+//  Created by Brian Kress on 5/29/14.
+//  Copyright (c) 2014 Brian Kress. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
